21fb563fe3581ef7824ce62c23a81f51222cef13
zz3014943854-ops <zz3014943854@163.com>
2026-06-16 22:43:45 +0800

    support cloudflare pages base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const isCloudflarePages = process.env.CF_PAGES === "1";
+
 export default defineConfig({
-  base: "/banhai-bao/",
+  base: isCloudflarePages ? "/" : "/banhai-bao/",
   plugins: [react()],
 });
